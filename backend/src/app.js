171c91f13ff4578407782a95e8ae8baebc7c924d
ethenkem <ethenatx@gmail.com>
2025-01-31 16:11:19 +0000

    cors

--- backend/src/app.js
+++ backend/src/app.js
@@ -3,10 +3,13 @@ import morgan from "morgan";
 import authRouter from "./auths/routes.js";
 import { TOKEN_KEY } from "./configs/constants.js";
 import { runMongoDbConfig } from "./configs/db.js";
+import cors from "cors"
 
 const app = express();
 
 runMongoDbConfig();
+
+app.use(cors())
 
 app.use(morgan("dev"));
 
--- frontend/src/pages/Login.jsx
+++ frontend/src/pages/Login.jsx
@@ -45,7 +45,7 @@ function Login({ showLoginModal, setShowLoginModal }) {
           </div>
           <p className="text-gray-600 mb-5">Please enter your details to sign in</p>
           {error && <p className='text-red-500 text-center text-sm'>{error}</p>}
-          <form className="space-y-3">
+          <div className="space-y-3">
             {/* Email Field */}
             <div>
               <label className="block text-sm font-medium text-gray-700 mb-2">
@@ -118,7 +118,7 @@ function Login({ showLoginModal, setShowLoginModal }) {
                 Sign in
               </button>
             </p>
-          </form>
+          </div>
         </div>
       </Box>
     </Modal>
